fe1848d9bead941959f41de149fa12f02e55848e
Stig Bakken <stig@stigbakken.com>
2026-04-16 13:12:07 +0200

    Rewrite server entry point with react/http, remove HttpFileServer

--- bin/server.php
+++ bin/server.php
@@ -1,8 +1,5 @@
-#!/bin/sh
-exec php -d output_buffering=1 -d display_errors=1 -d memory_limit=128M $0 $@
+#!/usr/bin/env php
 <?php
-
-ob_end_clean();
 
 /** @var DI\Container $container */
 $container = require dirname(__DIR__) . '/php/bootstrap.php';
@@ -12,26 +9,57 @@ if (isset($options['p'])) {
     $container->set('server.port', (int)$options['p']);
 }
 if (isset($options['f'])) {
-    $container->set('server.fps', (double)$options['f']);
+    $container->set('server.fps', (float)$options['f']);
 }
 
 $loop = React\EventLoop\Loop::get();
 $container->set(React\EventLoop\LoopInterface::class, $loop);
 
+$gameServer = $container->get(stigsb\pixelpong\server\GameServer::class);
+$wsHandler = new stigsb\pixelpong\server\WebSocketHandler($gameServer);
+$htdocsPath = dirname(__DIR__) . '/res/htdocs';
+
+$mimeTypes = [
+    'html' => 'text/html',
+    'css'  => 'text/css',
+    'js'   => 'application/javascript',
+    'json' => 'application/json',
+    'png'  => 'image/png',
+    'jpg'  => 'image/jpeg',
+    'gif'  => 'image/gif',
+    'svg'  => 'image/svg+xml',
+    'ico'  => 'image/x-icon',
+];
+
+$httpServer = new React\Http\HttpServer(function (Psr\Http\Message\ServerRequestInterface $request) use ($wsHandler, $htdocsPath, $mimeTypes) {
+    if (strtolower($request->getHeaderLine('Upgrade')) === 'websocket') {
+        return $wsHandler->handleUpgrade($request);
+    }
+
+    $path = $request->getUri()->getPath();
+    if ($path === '/') {
+        $path = '/index.html';
+    }
+
+    $file = $htdocsPath . $path;
+    $realFile = realpath($file);
+
+    if ($realFile && str_starts_with($realFile, realpath($htdocsPath)) && is_file($realFile)) {
+        $ext = pathinfo($realFile, PATHINFO_EXTENSION);
+        $mime = $mimeTypes[$ext] ?? 'application/octet-stream';
+        return new React\Http\Message\Response(
+            200,
+            ['Content-Type' => $mime],
+            file_get_contents($realFile)
+        );
+    }
+
+    return new React\Http\Message\Response(404, ['Content-Type' => 'text/plain'], 'Not Found');
+});
+
 $socket = new React\Socket\SocketServer($container->get('server.bind_addr') . ':' . $container->get('server.port'), [], $loop);
+$httpServer->listen($socket);
 
-$frame_buffer = $container->get(\stigsb\pixelpong\frame\FrameBuffer::class);
-//$game_server = new stigsb\pixelpong\server\GameServer($loop, $frame_buffer);
-$game_server = $container->get(stigsb\pixelpong\server\GameServer::class);
-
-$io_server = new Ratchet\Server\IoServer(
-    new Ratchet\Http\HttpServer(
-        new Ratchet\WebSocket\WsServer($game_server)
-    ),
-    $socket,
-    $loop
-);
-
-printf("Listening to port %d\n", $container->get('server.port'));
+printf("Listening on port %d\n", $container->get('server.port'));
 
 $loop->run();
